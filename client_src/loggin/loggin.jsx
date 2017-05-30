2e20e6ed9663683e10be8772f5da5f243ebc835f
iliyas <qwer@asdf.com>
2017-05-30 21:03:13 +0300

    добавлен бутстрэп

--- client_src/loggin/loggin.jsx
+++ client_src/loggin/loggin.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 export default class Loggin extends React.Component {
    render() {
       return (
-         <div>
-            Loggin
+         <div className='row'>
+
+              <div className='col-md-4'> COL 1</div>
+              <div className='col-md-4'> Loggin</div>
+              <div className='col-md-4'> COL 3</div>
          </div>
       );
    }
