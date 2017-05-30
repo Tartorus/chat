3aaddc1f99b93c0f7b2d4e64ff0ede9f646772f3
Tartorus <nclient@yandex.ru>
2017-05-30 15:38:40 +0300

    дщппшт

--- client_src/loggin/loggin.jsx
+++ client_src/loggin/loggin.jsx
@@ -4,7 +4,11 @@ export default class Loggin extends React.Component {
    render() {
       return (
          <div>
-            Loggin
+            <form action='api/loggin' method='POST'>
+              <input placeholder='loggin' type='text'/>
+              <p><input placeholder='password' type='password'/></p>
+              <p><input type='submit' value='Отправить'/></p>
+            </form>
          </div>
       );
    }
---
+++ corpchat_server/fixture_db.sql
@@ -0,0 +1,2 @@
+create user corpchat with password '100';
+create database corpchat with owner  corpchat;
