374ca47f5f50f5e00c1ecff2628c0dbd1917a9de
Tartorus <nclient@yandex.ru>
2017-06-01 13:04:57 +0300

    форма для логина

--- client_src/loggin/loggin.jsx
+++ client_src/loggin/loggin.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 
 export default class Loggin extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: '',
+      password: ''
+    };
+
+  }
+
+  handleChange(state) {
+    let that = this;
+    return function(event) {
+      let _state = {};
+      _state[state] =  event.target.value
+      that.setState(_state);
+    }
+}
+
+  handleSubmit(event) {
+    alert('A login was submitted: ' + this.state.login);
+    alert('A password was submitted: ' + this.state.password);
+    event.preventDefault();
+  }
+
+
    render() {
       return (
          <div>
-            <form action='api/loggin' method='POST'>
-              <input placeholder='loggin' type='text'/>
-              <p><input placeholder='password' type='password'/></p>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <input placeholder='login' type='text' value={this.state.login} onChange={this.handleChange.bind(this)('login')}/>
+              <p><input placeholder='password' type='password' value={this.state.password} onChange={this.handleChange.bind(this)('password')}/></p>
               <p><input type='submit' value='Отправить'/></p>
             </form>
          </div>
